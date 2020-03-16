893885615d58c6e033d389ac59649a2c5b62f75b
idevjinie <idevjinie@gmail.com>
2020-03-16 14:56:57 +0900

    Feat: 셸에 랜덤한 색상 적용하도록 기능 구현함

--- PhotosApp/PhotosApp/CollectionViewDataSource.swift
+++ PhotosApp/PhotosApp/CollectionViewDataSource.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+extension CGFloat {
+    
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+    
+}
+
+extension UIColor {
+    
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
+    
+}
+
 class CollectionViewDataSource: NSObject, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -16,7 +35,8 @@ class CollectionViewDataSource: NSObject, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
-        return cell 
+        cell.backgroundColor = .random()
+        return cell
     }
     
 }
