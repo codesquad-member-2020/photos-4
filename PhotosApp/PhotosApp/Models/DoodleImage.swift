c3099ed9c151733c23868d4d879c41e053f1f301
ehgud0670 <kimdo2297@gmail.com>
2020-03-18 17:25:49 +0900

    Feat: Codable을 채택한 모델 객체 DoodleImage 를 만든다. 모델 객체 DoodleImage 는 다음과 같은 속성을 갖는다.
    let order: Int
    let imageURL: URL
    let date: Date

---
+++ PhotosApp/PhotosApp/Models/DoodleImage.swift
@@ -0,0 +1,17 @@
+//
+//  DoodleImage.swift
+//  PhotosApp
+//
+//  Created by kimdo2297 on 2020/03/18.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
+
+struct DoodleImage: Codable {
+    
+    private let order: Int
+    private let imageURL: URL
+    private let date: Date
+    
+}
