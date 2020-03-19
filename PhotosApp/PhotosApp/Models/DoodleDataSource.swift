1d16545f46b7a1bae898eb6650bfec1a5820327a
ehgud0670 <kimdo2297@gmail.com>
2020-03-19 21:32:34 +0900

    Refactor: 다운받은 이미지 개수가 40개이거나 다 다운받은 경우에만 	 reloadData() 을 하도록 기능 구현함

--- PhotosApp/PhotosApp/Models/DoodleDataSource.swift
+++ PhotosApp/PhotosApp/Models/DoodleDataSource.swift
@@ -15,40 +15,37 @@ class DoodleDataSource: NSObject, UICollectionViewDataSource {
     private let doodleImageManager = DoodleImageManager()
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("doodleImageInfos?.count: \(String(describing: doodleImageInfos.count))")
         return doodleImageInfos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let doodleCell = collectionView.dequeueReusableCell(withReuseIdentifier:
             DoodleCell.reuseIdentifier, for: indexPath) as! DoodleCell
-        if doodleImages.count != 0 , indexPath.item < doodleImages.count {
+        if doodleImages.count != 0 ,
+            indexPath.item < doodleImages.count {
             doodleCell.setPhoto(image: doodleImages[indexPath.item])
         }
         return doodleCell
     }
     
-    func setupPhotos(resultHandler: @escaping () -> ()) {
+    func setupPhotos(reloadData: @escaping () -> ()) {
+        let numberOfItems = 40
         decodeDoodleImagesJSONData { doodleImageInfos in
-            var count = 0
+            var doodleCount = 0
             doodleImageInfos?.forEach({
                 self.doodleImageManager.downloadImage(urlString: $0.imageURLString) { image in
                     if let image = image {
                         self.doodleImages.append(image)
                     }
-                    count += 1
-                    if count == 40 {
-                        resultHandler()
-                        print(count)
-                    } else if count == self.doodleImageInfos.count {
-                        resultHandler()
-                        print(count)
+                    if (doodleCount == numberOfItems) ||
+                        (self.doodleImages.count == self.doodleImageInfos.count) {
+                        reloadData()
                     }
+                    doodleCount += 1
                 }
             })
         }
     }
-    
     
     private func decodeDoodleImagesJSONData(resultHandler: @escaping ([DoodleImageInfo]?) -> ()) {
         DataDecoder.decodeJSONData(from: URLInfo.addressAboutDoodleDatas,
