8b1782726318039d55d360d81326ca73491e977d
idevjinie <idevjinie@gmail.com>
2020-03-19 11:44:01 +0900

    Deleted: 테스트 용도인 테스트 프로젝트 삭제함

---
+++ PhotosApp/PhotosApp/Views/DoodleCell.swift
@@ -0,0 +1,13 @@
+//
+//  DoodleCell.swift
+//  PhotosApp
+//
+//  Created by jinie on 2020/03/19.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class DoodleCell: UICollectionViewCell {
+    
+}
